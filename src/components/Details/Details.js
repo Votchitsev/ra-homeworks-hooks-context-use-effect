3fe7e0da90a95d0cf09c466557a4aa483104436c
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-11-05 14:16:38 +0200

    Task complete

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
 import './Details.css';
 
 function Details( { info }) {
@@ -6,19 +7,26 @@ function Details( { info }) {
   const [details, setDetails] = useState({});
   const [userId, setUserId] = useState(info.id);
   const [avatar, setAvatar] = useState();
+  const [progress, setProgress] = useState(false);
 
   if (userId !== info.id) {
     setUserId(info.id);
   }
 
   useEffect(() => {
+    setProgress(true)
     fetch(process.env.REACT_APP_USER + `${userId}.json`)
       .then(response => response.json())
       .then(response => {
         setDetails(() => response.details);
         setAvatar(() => response.avatar);
+        setProgress(false)
       })
   }, [userId])
+
+  if (progress) {
+    return <CircularProgress />
+  } 
 
   return (
     <div className="details">
